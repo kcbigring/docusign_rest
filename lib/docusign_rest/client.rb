8cbe27866e5aba9841712e79db3fa5e43d88227d
kcbigring <kcbigring@gmail.com>
2014-10-27 15:35:19 -0600

    update recipient id

--- lib/docusign_rest/client.rb
+++ lib/docusign_rest/client.rb
@@ -317,7 +317,7 @@ module DocusignRest
           note:                                  '',
           phoneAuthentication:                   nil,
           recipientAttachment:                   nil,
-          recipientId:                           "#{index + 1}",
+          recipientId:                           "#{signer[:recipient_id]}" || "#{index + 1}",
           requireIdLookup:                       false,
           roleName:                              signer[:role_name],
           routingOrder:                          signer[:routing_order] || index + 1,
