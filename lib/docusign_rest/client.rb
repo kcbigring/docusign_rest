f1fef924f2abb91a6b38315de46cf549f9ce50c7
kcbigring <kcbigring@gmail.com>
2017-06-05 22:34:24 +0000

    composite template work

--- lib/docusign_rest/client.rb
+++ lib/docusign_rest/client.rb
@@ -529,8 +529,10 @@ module DocusignRest
       signers_array = []
       signers.each do |signer|
         signers_hash = Hash[:email, signer[:email], :name, signer[:name], \
-          :recipientId, signer[:recipient_id], :roleName, signer[:role_name], \
-          :clientUserId, signer[:email]]
+          :recipientId, signer[:recipient_id], :roleName, signer[:role_name]]
+        if signer[:embedded]
+          signers_hash[:clientUserId] = signer[:client_id] || signer[:email]
+        end
         signers_array << signers_hash
       end
       template_hash = Hash[:sequence, sequence, :recipients, { signers: signers_array }]
